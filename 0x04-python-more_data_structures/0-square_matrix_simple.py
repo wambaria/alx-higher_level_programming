bfa25d1e76ce122bd29a90c706acb6afc6307b48
wambaria <wambaria96@gmail.com>
2023-03-15 11:20:49 -0700

    Fixed 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,4 +5,4 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-    return (new_matrix)i
+    return (new_matrix)
