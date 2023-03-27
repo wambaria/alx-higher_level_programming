ba8b4a39d840861fa03980dba8343a0b370cefad
wambaria <wambaria96@gmail.com>
2023-03-27 11:25:31 -0700

    Added fixed file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
-        try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
-            break
-    print()
-    return n
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except(ValueError, TypeError):
+        return False
+    else:
+        return True
