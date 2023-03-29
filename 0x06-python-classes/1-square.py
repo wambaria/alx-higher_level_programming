18f97e2c357001ee2ceb662f8ed7b71a63f4be4b
wambaria <wambaria96@gmail.com>
2023-03-29 09:34:23 -0700

    Added file two

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class square is declared"""
+
+    def __init__(self, square_size):
+        """Constructor of the Square"""
+        self.__size = square_size
