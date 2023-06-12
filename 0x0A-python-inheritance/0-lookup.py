b54e782059f829f00394b05d34c7a3f5d401fd87
wambaria <wambaria96@gmail.com>
2023-06-12 10:50:11 -0700

    Aded 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@ contains def of the function lookup
 """
 
 def lookup(obj):
+
     """Returns list of attributes and methods of an object
     Args:
         obj (any): object whose attributes and methods are to be returned
